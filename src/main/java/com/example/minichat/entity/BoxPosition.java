49a4fd5c15b1dfff535e1d5d0d1ef378640af7b6
guanda.he <guanda.he@zhenai.com>
2018-09-18 21:58:44 +0800

    增加定位

--- src/main/java/com/example/minichat/entity/BoxPosition.java
+++ src/main/java/com/example/minichat/entity/BoxPosition.java
@@ -1,5 +1,7 @@
 package com.example.minichat.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 import java.io.Serializable;
 
@@ -11,6 +13,7 @@ import java.io.Serializable;
  * @author granda
  * @since 2018-09-10
  */
+@Data
 public class BoxPosition implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -37,81 +40,6 @@ public class BoxPosition implements Serializable {
     private Date updateTime;
 
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAddr() {
-        return addr;
-    }
-
-    public void setAddr(String addr) {
-        this.addr = addr;
-    }
-
-    public Integer getDeptId() {
-        return deptId;
-    }
-
-    public void setDeptId(Integer deptId) {
-        this.deptId = deptId;
-    }
-
-    public String getDeptName() {
-        return deptName;
-    }
-
-    public void setDeptName(String deptName) {
-        this.deptName = deptName;
-    }
-
-    public String getDeptAddr() {
-        return deptAddr;
-    }
-
-    public void setDeptAddr(String deptAddr) {
-        this.deptAddr = deptAddr;
-    }
-
-    public String getDistance() {
-        return distance;
-    }
-
-    public void setDistance(String distance) {
-        this.distance = distance;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "BoxPosition{" +
-        ", id=" + id +
-        ", addr=" + addr +
-        ", deptId=" + deptId +
-        ", deptName=" + deptName +
-        ", deptAddr=" + deptAddr +
-        ", distance=" + distance +
-        ", createTime=" + createTime +
-        ", updateTime=" + updateTime +
-        "}";
-    }
+    private String x;
+    private String y;
 }
