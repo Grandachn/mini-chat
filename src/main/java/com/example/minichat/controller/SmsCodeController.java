8a9fb17bcad34f5a594e1a46e5dac537c904d4f2
granda <grandachn@gmail.com>
2018-09-23 20:50:43 +0800

    add send mail

--- src/main/java/com/example/minichat/controller/SmsCodeController.java
+++ src/main/java/com/example/minichat/controller/SmsCodeController.java
@@ -10,12 +10,16 @@ import com.example.minichat.interceptor.SessionContext;
 import com.example.minichat.service.SmsCodeService;
 import com.example.minichat.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,6 +41,11 @@ public class SmsCodeController {
     @Autowired
     private UserInfoService userInfoService;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private ExecutorService sendMailThread = Executors.newFixedThreadPool(3);
+
     private Pattern pattern = Pattern.compile("1\\d{10}");
 
     @GetMapping("/{phone}")
@@ -53,6 +62,23 @@ public class SmsCodeController {
         }
         smsCode.setCode(verifyCode);
         smsCodeService.insertOrUpdate(smsCode);
+
+
+        SmsCode finalSmsCode = smsCode;
+        sendMailThread.execute(() -> {
+            SimpleMailMessage mainMessage = new SimpleMailMessage();
+            //发送者
+            mainMessage.setFrom("hgd0922@163.com");
+            //接收者
+            mainMessage.setTo("grandachn@163.com");
+            //发送的标题
+            mainMessage.setSubject("【珍心面对面】手机号：" + phone);
+            //发送的内容
+            mainMessage.setText("获取到的验证码为：" + finalSmsCode.getCode());
+
+            javaMailSender.send(mainMessage);
+        });
+
         return Result.success();
     }
     @GetMapping("/{phone}/{code}")
